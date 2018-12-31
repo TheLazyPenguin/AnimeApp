ff9aec017f662cb362a1fdaccae9f6b1fd538600
Lazyw <tigerclaw49@gmail.com>
2018-12-31 16:51:18 -0400

    Added Load.java and main.fxml

---
+++ src/sample/Load.java
@@ -0,0 +1,34 @@
+package sample;
+import javafx.animation.*;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.util.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+
+
+public class Load implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Timeline timeline = new Timeline(new KeyFrame(
+                Duration.millis(5000),
+                e -> {
+                    try {
+                       Parent root = FXMLLoader.load(getClass().getResource("Scenes/Main.fxml"));
+                        Scene main = new Scene(root,1920,1080);
+                        Main.changeWindow(main);
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }));
+        timeline.play();
+
+    }
+}
