14b79a3a4dac9ae40d88dca27ee460e2d0746f1e
Lazyw <tigerclaw49@gmail.com>
2019-01-02 02:26:43 -0400

    Zoom when mouse over image

--- src/sample/mainController.java
+++ src/sample/mainController.java
@@ -1,10 +1,13 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -16,7 +19,7 @@ import java.util.ResourceBundle;
 public class mainController implements Initializable {
     private ArrayList<ImageView> imagevw;
     @FXML
-    private ImageView imageView;
+    private ImageView imageView1;
     @FXML
     private ImageView imageView2;
     @FXML
@@ -35,11 +38,11 @@ public class mainController implements Initializable {
     private ImageView imageView9;
     @FXML
     private ImageView imageView10;
-
+    private int imgN;
 
     public ArrayList<ImageView> trending(){
         ArrayList<ImageView> imagevw = new ArrayList<ImageView>();
-        imagevw.add(imageView);
+        imagevw.add(imageView1);
         imagevw.add(imageView2);
         imagevw.add(imageView3);
         imagevw.add(imageView4);
@@ -50,6 +53,29 @@ public class mainController implements Initializable {
         imagevw.add(imageView9);
         imagevw.add(imageView10);
         return imagevw;
+    }
+    @FXML
+    private void imageZoom(Event e){
+        String foo = e.getSource().toString();
+        foo = foo.substring(foo.indexOf("=")+1);
+        foo = foo.substring(0,foo.indexOf(","));
+        foo = foo.substring(foo.indexOf("w")+1);
+        imgN = Integer.parseInt(foo)-1;
+
+        imagevw.get(imgN).setFitWidth(200);
+        imagevw.get(imgN).setFitHeight(225);
+
+
+
+    }
+
+    @FXML
+    private void imageDzoom(Event e){
+        imagevw.get(imgN).setFitWidth(144);
+        imagevw.get(imgN).setFitHeight(200);
+
+
+
     }
 
     @Override
